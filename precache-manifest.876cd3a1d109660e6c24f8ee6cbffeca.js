22378234c1c90101e7de54244a68985a709d5253
DylanBelka <dbelka@Dylan-PC.localdomain>
2020-04-04 00:12:32 -0400

    Updates

---
+++ precache-manifest.876cd3a1d109660e6c24f8ee6cbffeca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecfcaa7a9a9beac67523ed6256ec3197",
+    "url": "/annotweet-fe/index.html"
+  },
+  {
+    "revision": "8a9044bd490ebc58bd43",
+    "url": "/annotweet-fe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98e25004eb8ef00b92c8",
+    "url": "/annotweet-fe/static/js/2.84d96bd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/annotweet-fe/static/js/2.84d96bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a9044bd490ebc58bd43",
+    "url": "/annotweet-fe/static/js/main.f9bef4d5.chunk.js"
+  },
+  {
+    "revision": "a4e3a778668fb5e798c1",
+    "url": "/annotweet-fe/static/js/runtime-main.0d3fefcc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/annotweet-fe/static/media/logo.5d5d9eef.svg"
+  }
+]);
