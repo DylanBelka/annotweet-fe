90037066ed7337fd2064e200f88026a2fd07439c
DylanBelka <dbelka@Dylan-PC.localdomain>
2020-04-04 00:21:09 -0400

    Updates

---
+++ precache-manifest.3fe0f6ca2c7f3c75136cec601cbff807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e585c558583dee9340f64b1f1cc3ef3",
+    "url": "/annotweet-fe/index.html"
+  },
+  {
+    "revision": "8d82678dcbae85f390fe",
+    "url": "/annotweet-fe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "9da284e52cfdaccaad35",
+    "url": "/annotweet-fe/static/js/2.66ff2beb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/annotweet-fe/static/js/2.66ff2beb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d82678dcbae85f390fe",
+    "url": "/annotweet-fe/static/js/main.0afa2169.chunk.js"
+  },
+  {
+    "revision": "a4e3a778668fb5e798c1",
+    "url": "/annotweet-fe/static/js/runtime-main.0d3fefcc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/annotweet-fe/precache-manifest.876cd3a1d109660e6c24f8ee6cbffeca.js"
+  "/annotweet-fe/precache-manifest.3fe0f6ca2c7f3c75136cec601cbff807.js"
 );
 
 self.addEventListener('message', (event) => {
